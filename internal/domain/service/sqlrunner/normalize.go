32df82f8696debbe525da15205962e61af9489a5
Алексей Кузьмин <akuzmin340@gmail.com>
2026-04-24 11:44:22 +0200

    fix value normalize

--- internal/domain/service/sqlrunner/normalize.go
+++ internal/domain/service/sqlrunner/normalize.go
@@ -1,7 +1,10 @@
 package sqlrunner
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgtype"
+	"strconv"
 	"time"
 )
 
@@ -13,7 +16,15 @@ func normalizeDBValue(v any) string {
 		return string(t)
 	case time.Time:
 		return t.Format(time.RFC3339Nano)
+	case pgtype.Numeric:
+		jsVal, _ := t.Float64Value()
+		return strconv.FormatFloat(jsVal.Float64, 'f', -1, 64)
 	default:
+		if marshaler, ok := v.(json.Marshaler); ok {
+			if jsonVal, err := marshaler.MarshalJSON(); err == nil {
+				return string(jsonVal)
+			}
+		}
 		return fmt.Sprint(v)
 	}
 }
